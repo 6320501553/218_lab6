45df268b4710f141e9028fe6d635672cfc9c910c
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-18 15:45:39 +0700

    add

--- Game_218_6_2.c
+++ Game_218_6_2.c
@@ -3,44 +3,42 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    int num[2],k[2][n];
+    int num[2],k[2][n],sum=0,min=0;
     if(n>3&&n<10)
     {
-        for(i=0; i<=1; i++)
+        for(i=0; i<2; i++)
         {
             num[i]=0;
             scanf("%d",&num[i]);
         }
-        for(i=0;i<=1;i++)
+        for(i=0; i<=1; i++)
         {
-            for(j=0;j<=n;j++)
+            for(j=0; j<=n; j++)
                 k[i][j]=0;
         }
-        i=0;
-        j=0;
-        while(1)
+        for(i=0; i<2; i++)
         {
-            if(num[i]!=0)
+            for(j=0; j<n; j++)
             {
-                k[i][j]=num[i]%10;
-                num[i]/=10;
-                j++;
+                if(num[i]!=0)
+                {
+                    k[i][j]=num[i]%10;
+                    num[i]/=10;
+                }
             }
-            else if(i<=1)
-            {
-                i++;
-            }
-            else
-                break;
-        }
-        for(i=0;i<=1;i++)
-        {
-            for(j=n-1;j>=0;j--)
-            {
-                printf("%d",k[i][j]);
-            }
-            printf("\n");
         }
     }
+    for(j=n-1; j>=0; j--)
+    {
+        if(k[0][j]!=k[1][j])
+        {
+            min++;
+        }
+        if(k[0][j]==k[1][j])
+        {
+            sum++;
+        }
+    }
+    printf("%d %d",sum,min);
     return 0;
 }
